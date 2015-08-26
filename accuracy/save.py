cac892f8e687a77dc8c4be00366ddb40c54cfbf5
Joscha Reimer <jor@informatik.uni-kiel.de>
2015-08-26 15:24:00 +0200

    ENH: ndop/accuracy/save.py: add GLS support

--- accuracy/save.py
+++ accuracy/save.py
@@ -2,80 +2,76 @@ import argparse
 import sys
 import numpy as np
 
-from ndop.accuracy.asymptotic import OLS, WLS, GLS
+import ndop.optimization.constants
+import ndop.accuracy.asymptotic
 from util.logging import Logger
 
 from ndop.constants import MODEL_OUTPUT_DIR
+from ndop.optimization.matlab.constants import KIND_OF_COST_FUNCTIONS
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Calculating accuracy.')
 
-    parser.add_argument('-d', '--kind_of_data', choices=('WOA', 'WOD'), default='WOA', help='The kind of the data to chose.')
-    parser.add_argument('-c', '--kind_of_cost_function', choices=('OLS', 'WLS', 'GLS'), default='OLS', help='The kind of the cost function to chose.')
+    # parser.add_argument('-d', '--data_kind', choices=('WOA', 'WOD'), default='WOA', help='The kind of the data to chose.')
+    # parser.add_argument('-c', '--cf_kind', choices=('OLS',), default='OLS', help='The kind of the cost function to chose.')
+    parser.add_argument('-k', '--kind', choices=KIND_OF_COST_FUNCTIONS, help='The kind of the cost function to chose.')
     parser.add_argument('-p', '--parameter_set_nr', type=int, default=184, help='Parameter set nr.')
     parser.add_argument('-t', '--time_dim_df', type=int, default=2880, help='Time dim of df.')
 
     parser.add_argument('-i', '--number_of_measurements', type=int, default=1, help='Number of measurements for increase calulation.')
 
-#     parser.add_argument('-s', '--skip_increase', action='store_true', help='Skip increase calulation.')
     parser.add_argument('-m', '--use_mem_map', action='store_true', help='Use memmap to decrease memory use.')
 
     parser.add_argument('-n', '--not_parallel', action='store_false', help='Calculate serial.')
 
-    parser.add_argument('-v', '--value_mask_file', help='Calculate average model confidence increase with this value mask.')
+    parser.add_argument('-v', '--value_mask_file', default=None, help='Calculate average model confidence increase with this value mask.')
     parser.add_argument('-o', '--output_file', help='Save average model confidence increase to this file.')
 
     parser.add_argument('--version', action='version', version='%(prog)s 0.1')
 
-    args = vars(parser.parse_args())
-    print(args)
-
-    data_kind = args['kind_of_data']
-    cf_kind = args['kind_of_cost_function']
-    parameter_set_nr = args['parameter_set_nr']
-    time_dim_df = args['time_dim_df']
-
-    number_of_measurements = args['number_of_measurements']
-    use_mem_map = args['use_mem_map']
-    parallel = args['not_parallel']
-
-    output_file = args['output_file']
-    value_mask_file = args['value_mask_file']
-    if value_mask_file is not None:
-        value_mask = np.load(value_mask_file)
-    else:
-        value_mask = None
-#     try:
-#         output_file = args['output_file']
-#     except KeyError:
-#         output_file = None
-#     try:
-#         value_mask_file = args['value_mask_file']
-#         value_mask = np.load(value_mask_file)
-#     except KeyError:
-#         value_mask = None
-
-    if cf_kind == 'OLS':
-        cf_class = OLS
-    elif cf_kind == 'WLS':
-        cf_class = WLS
-    elif cf_kind == 'GLS':
-        cf_class = GLS
+    args = parser.parse_args()
 
     with Logger():
-        p = np.loadtxt(MODEL_OUTPUT_DIR+'/time_step_0001/parameter_set_{:0>5}/parameters.txt'.format(parameter_set_nr))
-        spinup_options = spinup_options={'years':10000, 'tolerance':0.0, 'combination':'or'}
-#         job_options = {'spinup': {'nodes_setup': ('f_ocean2', 5, 16)}, 'derivative': {'nodes_setup': ('foexpress', 2, 16)}, 'trajectory': {'nodes_setup': ('foexpress', 1, 16)}}
-        job_options = {'spinup': {'nodes_setup': ('f_ocean2', 5, 16)}, 'derivative': {'nodes_setup': ('foexpress', 2, 16)}, 'trajectory': {'nodes_setup': ('westmere', 1, 1)}}
+        ## extract infos from kind
+        kind_splitted = args.kind.split('.')
+        data_kind = kind_splitted[0]
+        cf_kind = kind_splitted[1]
 
-        asymptotic = cf_class(data_kind, spinup_options, job_setup=job_options)
+        if cf_kind == 'OLS':
+            cf_class = ndop.accuracy.asymptotic.OLS
+        elif cf_kind == 'WLS':
+            cf_class = ndop.accuracy.asymptotic.WLS
+        elif cf_kind == 'LWLS':
+            cf_class = ndop.accuracy.asymptotic.LWLS
+        elif cf_kind == 'GLS':
+            cf_class = ndop.accuracy.asymptotic.GLS
+            correlation_min_values = int(kind_splitted[2])
+            correlation_max_year_diff = int(kind_splitted[3])
+            if correlation_max_year_diff < 0:
+                correlation_max_year_diff = float('inf')
+        else:
+            raise ValueError('Unknown cf kind {}.'.format(cf_kind))
+        
+        ## init asymptotic
+        spinup_options = {'years':10000, 'tolerance':0.0, 'combination':'or'}
+        job_options = {'spinup': {'nodes_setup': ndop.optimization.constants.COST_FUNCTION_NODES_SETUP_SPINUP}, 'derivative': {'nodes_setup': ndop.optimization.constants.COST_FUNCTION_NODES_SETUP_DERIVATIVE}, 'trajectory': {'nodes_setup': ndop.optimization.constants.COST_FUNCTION_NODES_SETUP_TRAJECTORY}}
+        
+        cf_kargs = {'data_kind': data_kind, 'spinup_options': spinup_options, 'job_setup': job_options}
+        if cf_kind == 'GLS':
+            cf_kargs['correlation_min_values'] = correlation_min_values
+            cf_kargs['correlation_max_year_diff'] = correlation_max_year_diff
+        
+        asymptotic = cf_class(**cf_kargs)
+        
+        ## calculate
+        p = np.loadtxt(MODEL_OUTPUT_DIR+'/time_step_0001/parameter_set_{:0>5}/parameters.txt'.format(args.parameter_set_nr))
         asymptotic.parameter_confidence(p)
-        asymptotic.model_confidence(p, time_dim_df=time_dim_df, use_mem_map=use_mem_map, parallel_mode=parallel)
-        asymptotic.average_model_confidence(p, time_dim_df=time_dim_df, use_mem_map=use_mem_map, parallel_mode=parallel)
-        if number_of_measurements > 0:
-            average_model_confidence_increase = asymptotic.average_model_confidence_increase(p, number_of_measurements=number_of_measurements, time_dim_df=time_dim_df, value_mask=value_mask, use_mem_map=use_mem_map, parallel_mode=parallel)
-            if output_file is not None:
+        asymptotic.model_confidence(p, time_dim_df=args.time_dim_df, use_mem_map=args.use_mem_map, parallel_mode=not args.not_parallel)
+        asymptotic.average_model_confidence(p, time_dim_df=args.time_dim_df, use_mem_map=args.use_mem_map, parallel_mode=not args.not_parallel)
+        if args.number_of_measurements > 0:
+            average_model_confidence_increase = asymptotic.average_model_confidence_increase(p, number_of_measurements=args.number_of_measurements, time_dim_df=args.time_dim_df, value_mask=args.value_mask_file, use_mem_map=args.use_mem_map, parallel_mode=not args.not_parallel)
+            if args.output_file is not None:
                 np.save(output_file, average_model_confidence_increase)
 
         print('finished')
