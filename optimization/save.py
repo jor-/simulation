1e6382d2515eeee44b7c032fe8d6805afc33ef6d
Joscha Reimer <jor@informatik.uni-kiel.de>
2015-09-03 13:57:53 +0200

    MAINT: ndop/optimization/save_values.py -> ndop/optimization/save.py

--- optimization/save.py
+++ optimization/save.py
@@ -20,7 +20,6 @@ from ndop.constants import BASE_DIR
 def save(parameter_sets=range(9999), data_kind='WOA', eval_f=True, eval_df=True, as_jobs=False):
     from ndop.model.constants import MODEL_OUTPUT_DIR, MODEL_TIME_STEP_DIRNAME, MODEL_PARAMETERS_SET_DIRNAME, MODEL_PARAMETERS_FILENAME, MODEL_SPINUP_DIRNAME, MODEL_DERIVATIVE_DIRNAME
 
-
     for parameter_set_number in parameter_sets:
 
         ## get cf family
@@ -51,7 +50,6 @@ def save(parameter_sets=range(9999), data_kind='WOA', eval_f=True, eval_df=True,
                 cf_kargs = {'data_kind':data_kind, 'spinup_options':{'years':years, 'tolerance':tolerance, 'combination':'and'}, 'df_accuracy_order':df_accuracy_order, 'job_setup':{'name': 'SCF_' + data_kind}}
                 cost_function_family = ndop.optimization.cost_function.Family(**cf_kargs)
 
-
         ## eval cf family
         if cost_function_family is not None:
             p = np.loadtxt(parameters_file)
@@ -76,7 +74,7 @@ def save(parameter_sets=range(9999), data_kind='WOA', eval_f=True, eval_df=True,
                             from util.constants import TMP_DIR
                             output_dir = tempfile.TemporaryDirectory(dir=TMP_DIR, prefix='save_value_cost_function_tmp_').name
                             cf_kargs = cf.kargs
-                            cf_kargs['job_setup'] = {'name': '{}:{}'.format(cf, parameter_set_number)}
+                            cf_kargs['job_setup'] = {'name': 'save_{}:{}'.format(cf, parameter_set_number)}
                             with ndop.optimization.job.CostFunctionJob(output_dir, p, cf.kind, eval_f=eval_f, eval_df=eval_df, write_output_file=False, **cf_kargs) as cf_job:
                                 cf_job.start()
                             time.sleep(10)
@@ -98,15 +96,12 @@ if __name__ == "__main__":
     parser.add_argument('--DF', action='store_true', help='Eval (also) DF.')
     parser.add_argument('--version', action='version', version='%(prog)s 0.1')
 
-    args = vars(parser.parse_args())
-    data_kind = args['kind_of_cost_function']
-    first = args['first']
-    last = args['last']
-    debug = args['debug']
-    eval_df = args['DF']
-    as_jobs = args['as_jobs']
+    args = parser.parse_args()
+    parameter_sets = range(args.first, args.last+1)
+    
+    if args.as_jobs:
+        ndop.optimization.constants.COST_FUNCTION_NODES_SETUP_JOB = util.batch.universal.system.NodeSetup(memory=31, node_kind='clfocean', nodes=1, cpus=1, total_cpus_max=1, walltime=20)
+        ndop.optimization.constants.COST_FUNCTION_NODES_SETUP_TRAJECTORY = util.batch.universal.system.NodeSetup(memory=ndop.optimization.constants.JOB_MEMORY_GB, node_kind='clexpress', nodes=1, cpus=16, nodes_max=1, check_for_better=True, walltime=1)
 
-    parameter_sets = range(first, last+1)
-
-    with util.logging.Logger(disp_stdout=debug):
-        save(parameter_sets=parameter_sets, data_kind=data_kind, eval_df=eval_df, as_jobs=as_jobs)
+    with util.logging.Logger(disp_stdout=args.debug):
+        save(parameter_sets=parameter_sets, data_kind=args.kind_of_cost_function, eval_df=args.DF, as_jobs=args.as_jobs)
