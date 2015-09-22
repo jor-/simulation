d38127544a9cc5fc57e32a981308993661ea8647
Joscha Reimer <jor@informatik.uni-kiel.de>
2015-09-22 14:42:10 +0200

    MAINT: ndop.constants: BASE_DIR_ENV_NAME added

--- constants.py
+++ constants.py
@@ -2,7 +2,9 @@ import os.path
 
 import util.io.env
 
-BASE_DIR = util.io.env.load('NDOP_DIR')
+
+BASE_DIR_ENV_NAME = 'NDOP_DIR'
+BASE_DIR = util.io.env.load(BASE_DIR_ENV_NAME)
 
 MODEL_OUTPUT_DIR = os.path.join(BASE_DIR, 'model_output')
 PARAMETER_OPTIMIZATION_DIR = os.path.join(BASE_DIR, 'parameter_optimization')
