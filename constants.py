a10ff3205a614cdbea9cb941681af78c8f628bbc
Joscha Reimer <jor@informatik.uni-kiel.de>
2016-03-15 11:01:31 +0100

    ENH: constants: remove necessity of BASE_DIR env variable

--- constants.py
+++ constants.py
@@ -4,7 +4,12 @@ import util.io.env
 
 
 BASE_DIR_ENV_NAME = 'NDOP_DIR'
-BASE_DIR = util.io.env.load(BASE_DIR_ENV_NAME)
+try:
+    BASE_DIR = util.io.env.load(BASE_DIR_ENV_NAME)
+    PARAMETER_OPTIMIZATION_DIR = os.path.join(BASE_DIR, 'parameter_optimization')
+    OED_DIR = os.path.join(BASE_DIR, 'optimal_experimental_design')
+except util.io.env.EnvironmentLookupError:
+    pass
 
 
 MODEL_OUTPUT_ENV_NAME = 'MODEL_OUTPUT_DIR'
@@ -20,5 +25,3 @@ except util.io.env.EnvironmentLookupError:
     METOS3D_DIR = os.path.join(BASE_DIR, 'metos3d', 'fixed_version')
 
 
-PARAMETER_OPTIMIZATION_DIR = os.path.join(BASE_DIR, 'parameter_optimization')
-OED_DIR = os.path.join(BASE_DIR, 'optimal_experimental_design')
