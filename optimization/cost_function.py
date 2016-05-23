30134ff71d9187961875b9e0c22e6bf2292efbd9
Joscha Reimer <jor@informatik.uni-kiel.de>
2016-05-23 15:41:12 +0200

    MAINT: optimization.cost_function.Family: support for some new cost functions added

--- optimization/cost_function.py
+++ optimization/cost_function.py
@@ -558,11 +558,9 @@ class Family(simulation.util.data_base.Family):
    
     member_classes = {'WOA': [(OLS, [{}]), (WLS, [{}]), (LWLS, [{}])], 
                       'WOD': [(OLS, [{}]), (WLS, [{}]), (LWLS, [{}]), (GLS, [{'correlation_min_values': correlation_min_values, 'correlation_max_year_diff': float('inf')} for correlation_min_values in (50, 45,)])],
-                      'WOD.1': [(OLS, [{}]), (WLS, [{}]), (LWLS, [{}]), (GLS, [{'correlation_min_values': correlation_min_values, 'correlation_max_year_diff': float('inf')} for correlation_min_values in (50, 45, 40,)])],
-                      'WOD.0': [(OLS, [{}]), (WLS, [{}]), (LWLS, [{}]), (GLS, [{'correlation_min_values': correlation_min_values, 'correlation_max_year_diff': float('inf')} for correlation_min_values in (50, 45, 40,)])]
+                      'WOD.1': [(OLS, [{}]), (WLS, [{}]), (LWLS, [{}]), (GLS, [{'correlation_min_values': correlation_min_values, 'correlation_max_year_diff': float('inf')} for correlation_min_values in (50, 45, 40, 35)])],
+                      'WOD.0': [(OLS, [{}]), (WLS, [{}]), (LWLS, [{}]), (GLS, [{'correlation_min_values': correlation_min_values, 'correlation_max_year_diff': float('inf')} for correlation_min_values in (50, 45, 40, 35)])]
                       } 
-   
-    # member_classes = {'WOD.1': [(GLS, [{'correlation_min_values': correlation_min_values, 'correlation_max_year_diff': float('inf')} for correlation_min_values in (40, 45, 50)]), ],}
 
     def f(self, parameters):
         fun = lambda o: o.f(parameters)
