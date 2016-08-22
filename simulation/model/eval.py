97ff7d10e3ee43a8727c8f83e2a59cf405e3902f
Joscha Reimer <jor@informatik.uni-kiel.de>
2016-08-22 15:26:23 +0200

    BUG: simulation.model.eval: df now correctly returns empty array if no tracer is chosen

--- simulation/model/eval.py
+++ simulation/model/eval.py
@@ -784,9 +784,10 @@ class Model_With_F_And_DF(Model_With_F):
     
 
     def _df(self, trajectory_load_function, partial_derivative_kind, tracers=None):
+        ## check tracers
+        tracers = self.check_tracers(tracers)
 
         ## apply partial_derivative_kind
-
         if partial_derivative_kind == 'model_parameters':
             def convert_partial_derivative_parameters_to_start_run_parameters(partial_derivative_parameters):
                 assert len(partial_derivative_parameters) == self.model_options.parameters_len
@@ -808,6 +809,9 @@ class Model_With_F_And_DF(Model_With_F):
         else:
             raise ValueError('Partial derivative kind {} is not supported.'.format(partial_derivative_kind))
         
+        ## return empty array if no tracer wanted
+        if len(tracers) == 0:
+            return np.empty([len(partial_derivative_parameters_bounds), 0])
         
         ## get needed model options
         MODEL_DERIVATIVE_SPINUP_YEARS = self.model_options.derivative_options.years
@@ -894,7 +898,6 @@ class Model_With_F_And_DF(Model_With_F):
             return 0
             
 
-        tracers = self.check_tracers(tracers)
         tracer_start_stop_indices = [0]
         
         def get_partial_derivative_run_value(partial_derivative_parameters):
