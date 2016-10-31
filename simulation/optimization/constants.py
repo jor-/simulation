a421cbfbafc093a521cfc806d18bdfb8f87ea392
Joscha Reimer <jor@informatik.uni-kiel.de>
2016-10-31 09:31:20 +0100

    MAINT: simulation.optimization.constants: default spinup queue changed

--- simulation/optimization/constants.py
+++ simulation/optimization/constants.py
@@ -19,7 +19,7 @@ if util.batch.universal.system.IS_RZ:
     COST_FUNCTION_NODES_SETUP_JOB = util.batch.universal.system.NodeSetup(memory=30, node_kind='f_ocean2', nodes=1, cpus=1, total_cpus_max=1, walltime=48, check_for_better=True)
 
 if util.batch.universal.system.IS_NEC:
-    COST_FUNCTION_NODES_SETUP_SPINUP = util.batch.universal.system.NodeSetup(memory=JOB_MEMORY_GB, node_kind='clfocean', nodes=4, cpus=16, total_cpus_max=8*24, check_for_better=True)
+    COST_FUNCTION_NODES_SETUP_SPINUP = util.batch.universal.system.NodeSetup(memory=JOB_MEMORY_GB, node_kind='clmedium', nodes=4, cpus=16, total_cpus_max=8*24, check_for_better=True)
     COST_FUNCTION_NODES_SETUP_DERIVATIVE = util.batch.universal.system.NodeSetup(memory=JOB_MEMORY_GB, node_kind='clmedium', nodes=4, cpus=16, total_cpus_max=8*24, check_for_better=True)
     COST_FUNCTION_NODES_SETUP_TRAJECTORY = util.batch.universal.system.NodeSetup(memory=JOB_MEMORY_GB, node_kind='clexpress', nodes=1, cpus=1, nodes_max=1, total_cpus_max=16, check_for_better=True, walltime=1)
     COST_FUNCTION_NODES_SETUP_JOB = util.batch.universal.system.NodeSetup(memory=30, node_kind='clmedium', nodes=1, cpus=1, total_cpus_max=1, walltime=48, check_for_better=True)
