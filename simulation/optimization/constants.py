9bf68530fdde411c029d30167f6d55b892f79a14
Joscha Reimer <jor@informatik.uni-kiel.de>
2017-10-19 12:16:00 +0200

    MAINT: simulation.optimization.constants: check_for_better enabled for spinup an derivatives on nec

--- simulation/optimization/constants.py
+++ simulation/optimization/constants.py
@@ -2,12 +2,17 @@ import util.batch.universal.system
 
 from simulation.model.constants import JOB_MEMORY_GB
 
+# cache file names
+
 COST_FUNCTION_DIRNAME = 'cost_functions'
 COST_FUNCTION_F_FILENAME = 'f_-_normalized_{normalized}_.npy'
 COST_FUNCTION_DF_FILENAME = 'df_-_normalized_{normalized}_-_{derivative_kind}.npy'
 
+# minimal concentration for log normal distributions
+
 CONCENTRATION_MIN_VALUE = 10**(-6)
 
+# node setups
 
 if util.batch.universal.system.IS_RZ:
     COST_FUNCTION_NODES_SETUP_SPINUP = util.batch.universal.system.NodeSetup(memory=JOB_MEMORY_GB, node_kind='f_ocean2', nodes=6, cpus=16, nodes_max=9, check_for_better=True)
@@ -16,11 +21,7 @@ if util.batch.universal.system.IS_RZ:
     COST_FUNCTION_NODES_SETUP_JOB = util.batch.universal.system.NodeSetup(memory=30, node_kind='f_ocean2', nodes=1, cpus=1, total_cpus_max=1, walltime=48, check_for_better=True)
 
 if util.batch.universal.system.IS_NEC:
-#    COST_FUNCTION_NODES_SETUP_SPINUP = util.batch.universal.system.NodeSetup(memory=JOB_MEMORY_GB, node_kind='clmedium', nodes=4, cpus=32, nodes_max=8, check_for_better=True)
-#    COST_FUNCTION_NODES_SETUP_DERIVATIVE = util.batch.universal.system.NodeSetup(memory=JOB_MEMORY_GB, node_kind='clmedium', nodes=4, cpus=32, nodes_max=8, check_for_better=True)
-#    COST_FUNCTION_NODES_SETUP_TRAJECTORY = util.batch.universal.system.NodeSetup(memory=JOB_MEMORY_GB, node_kind='clexpress', nodes=1, cpus=1, nodes_max=1, walltime=2, check_for_better=True)
-#    COST_FUNCTION_NODES_SETUP_JOB = util.batch.universal.system.NodeSetup(memory=30, node_kind='clmedium', nodes=1, cpus=1, total_cpus_max=1, walltime=48, check_for_better=True)
-    COST_FUNCTION_NODES_SETUP_SPINUP = util.batch.universal.system.NodeSetup(memory=JOB_MEMORY_GB, node_kind='clmedium', nodes=8, cpus=32, nodes_max=8, check_for_better=False)
-    COST_FUNCTION_NODES_SETUP_DERIVATIVE = util.batch.universal.system.NodeSetup(memory=JOB_MEMORY_GB, node_kind='clmedium', nodes=4, cpus=32, nodes_max=8, check_for_better=False)
+    COST_FUNCTION_NODES_SETUP_SPINUP = util.batch.universal.system.NodeSetup(memory=JOB_MEMORY_GB, node_kind='clmedium', nodes=8, cpus=32, nodes_max=8, check_for_better=True)
+    COST_FUNCTION_NODES_SETUP_DERIVATIVE = util.batch.universal.system.NodeSetup(memory=JOB_MEMORY_GB, node_kind='clmedium', nodes=4, cpus=32, nodes_max=8, check_for_better=True)
     COST_FUNCTION_NODES_SETUP_TRAJECTORY = util.batch.universal.system.NodeSetup(memory=JOB_MEMORY_GB, node_kind='clexpress', nodes=1, cpus=1, nodes_max=1, walltime=2, check_for_better=False)
     COST_FUNCTION_NODES_SETUP_JOB = util.batch.universal.system.NodeSetup(memory=30, node_kind='clmedium', nodes=1, cpus=1, total_cpus_max=1, walltime=48, check_for_better=False)
