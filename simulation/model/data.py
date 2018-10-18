cad2a67c985336b5fb1785eb3e2c25df097cab0d
Joscha Reimer <jor@informatik.uni-kiel.de>
2018-10-18 09:58:48 +0200

    BUG: simulation.model.data: load_trajectories_to_universal: trajectory is now completely loaded

--- simulation/model/data.py
+++ simulation/model/data.py
@@ -55,8 +55,8 @@ def convert_3D_to_metos_1D(data):
 
 # load trajectory
 
-def load_trajectories_to_universal(path, convert_function=None, converted_result_shape=None, tracers=None, time_dim_desired=None, set_negative_values_to_zero=False):
-    util.logging.debug('Loading trajectories with tracers {}, desired time dim {}, set_negative_values_to_zero {} and convert function {} with result shape {} from {}.'.format(tracers, time_dim_desired, set_negative_values_to_zero, convert_function, converted_result_shape, path))
+def load_trajectories_to_universal(path, tracers, convert_function=None, converted_result_shape=None, time_dim_desired=None, set_negative_values_to_zero=False):
+    util.logging.debug(f'Loading trajectories with tracers {tracers}, desired time dim {time_dim_desired}, set_negative_values_to_zero {set_negative_values_to_zero} and convert function {convert_function} with result shape {converted_result_shape} from {path}.')
 
     # check input
     if isinstance(tracers, str):
@@ -68,39 +68,42 @@ def load_trajectories_to_universal(path, convert_function=None, converted_result
             return x
 
         if converted_result_shape is not None:
-            raise ValueError('The convert function is None but the converted result shape is not None ({}).'.format(converted_result_shape))
+            raise ValueError(f'The convert function is None but the converted result shape is not None ({converted_result_shape}).')
     elif not callable(convert_function):
-        raise ValueError('The convert function {} has to be callable.'.format(convert_function))
+        raise ValueError(f'The convert function {convert_function} has to be callable.')
 
     # calculate tracer_time_dim
-    tracer_time_dim_found = False
-    time_dims = simulation.model.constants.METOS_TIME_STEPS
-    assert all(a < b for a, b in zip(time_dims[:-1], time_dims[1:]))
-    i = len(time_dims) - 1
-    while (not tracer_time_dim_found) and i >= 0:
-        tracer_time_dim = time_dims[i]
+    maximal_t_dim = simulation.model.constants.METOS_T_DIM
+    possible_time_dims = maximal_t_dim / np.sort(simulation.model.constants.METOS_TIME_STEPS)
+    possible_time_dims = possible_time_dims.astype(np.min_scalar_type(maximal_t_dim))
+
+    for tracer_time_dim in possible_time_dims:
         filename = simulation.model.constants.METOS_TRAJECTORY_FILENAME.format(tracer=tracers[0], time_step=tracer_time_dim - 1)
         file = os.path.join(path, filename)
         if os.path.exists(file):
-            tracer_time_dim_found = True
-        else:
-            i -= 1
-    if i < 0:
-        raise FileNotFoundError('No PETSc vectors found in {}.'.format(path))
+            break
+    else:
+        raise FileNotFoundError(f'No PETSc vectors found in {path}.')
 
-    util.logging.debug('{} petsc vectors were found for each tracer.'.format(tracer_time_dim_found))
+    assert os.path.exists(os.path.join(path, simulation.model.constants.METOS_TRAJECTORY_FILENAME.format(tracer=tracers[0], time_step=tracer_time_dim - 1)))
+    if os.path.exists(os.path.join(path, simulation.model.constants.METOS_TRAJECTORY_FILENAME.format(tracer=tracers[0], time_step=tracer_time_dim))):
+        raise ValueError(f'Trajectory in {path} is incomplete.')
+
+    util.logging.debug(f'{tracer_time_dim} petsc vectors were found for each tracer.')
 
     # calculate time_step, check time_dim_desired
     if time_dim_desired is not None:
-        if tracer_time_dim % time_dim_desired == 0:
-            time_step = int(tracer_time_dim / time_dim_desired)
+        time_step = tracer_time_dim / time_dim_desired
+        if time_step.is_integer():
+            time_step = int(time_step)
         else:
-            raise ValueError('The desired time dimension {0} can not be satisfied because the tracer time dimension {1} is not divisible by {0}.'.format(time_dim_desired, tracer_time_dim))
+            raise ValueError(f'The desired time dimension {time_dim_desired} can not be satisfied because the tracer time dimension {tracer_time_dim} is not intiger divideable by {time_dim_desired}.')
     else:
         time_dim_desired = tracer_time_dim
         time_step = 1
 
     assert tracer_time_dim % time_dim_desired == 0
+    assert tracer_time_dim == time_dim_desired * time_step
 
     # init trajectory
     if converted_result_shape is None:
@@ -114,12 +117,12 @@ def load_trajectories_to_universal(path, convert_function=None, converted_result
     trajectory = np.zeros(trajectory_shape, dtype=np.float64)
 
     # load and calculate trajectory
-    util.logging.debug('Loading trajectories from {} to array of size {}.'.format(path, trajectory.shape))
+    util.logging.debug(f'Loading trajectories from {path} to array of size {trajectory.shape}.')
 
     for tracers_index in range(tracers_len):
         tracer = tracers[tracers_index]
 
-        util.logging.debug('Loading trajectory for tracer {}.'.format(tracer))
+        util.logging.debug(f'Loading trajectory for tracer {tracer}.')
         for time_index in range(time_dim_desired):
             # average trajectory
             for k in range(time_step):
@@ -131,7 +134,7 @@ def load_trajectories_to_universal(path, convert_function=None, converted_result
                 # load vector and average
                 vec = util.petsc.universal.load_petsc_vec_to_numpy_array(file)
                 if np.any(np.isnan(vec)):
-                    raise ValueError('Trajectory {} contains nans.'.format(file))
+                    raise ValueError(f'Trajectory {file} contains nans.')
                 if set_negative_values_to_zero:
                     vec[vec < 0] = 0
                 if k == 0:
@@ -147,14 +150,14 @@ def load_trajectories_to_universal(path, convert_function=None, converted_result
 
             trajectory[tracers_index, time_index] = trajectory_averaged
 
-    util.logging.debug('Trajectory with shape {} loaded.'.format(trajectory.shape))
+    util.logging.debug(f'Trajectory with shape {trajectory.shape} loaded.')
 
     return trajectory
 
 
 def load_trajectories_to_map(path, tracers, time_dim_desired=None):
     # load trajectory
-    trajectory = load_trajectories_to_universal(path, convert_function=convert_metos_1D_to_3D, converted_result_shape=simulation.model.constants.METOS_SPACE_DIM, tracers=tracers, time_dim_desired=time_dim_desired)
+    trajectory = load_trajectories_to_universal(path, tracers, convert_function=convert_metos_1D_to_3D, converted_result_shape=simulation.model.constants.METOS_SPACE_DIM, time_dim_desired=time_dim_desired)
     trajectory = trajectory[0]
 
     assert trajectory.ndim == 4
@@ -182,7 +185,7 @@ def load_trajectories_to_map_index_array(path, tracers, time_dim_desired=None):
 
         return data
 
-    trajectory = load_trajectories_to_universal(path, convert_function=convert_function, tracers=tracers, time_dim_desired=time_dim_desired)
+    trajectory = load_trajectories_to_universal(path, tracers, convert_function=convert_function, time_dim_desired=time_dim_desired)
     trajectory = trajectory[0]
     assert trajectory.ndim == 3
     assert np.all(trajectory[:, :, :3] % 1 == 0)
