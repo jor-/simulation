fec32937e6bf8134076a1a2178a0d1f08d130c00
Joscha Reimer <jor@informatik.uni-kiel.de>
2019-02-20 14:32:35 +0100

    MAINT: simulation.optimization.constants: use clmedium instead of clexpress for trajectories on NEC

--- simulation/optimization/constants.py
+++ simulation/optimization/constants.py
@@ -23,5 +23,5 @@ if util.batch.universal.system.IS_RZ:
 if util.batch.universal.system.IS_NEC:
     COST_FUNCTION_NODES_SETUP_SPINUP = util.batch.universal.system.NodeSetup(memory=JOB_MEMORY_GB, node_kind='clmedium', nodes=4, cpus=32, nodes_max=8, check_for_better=True)
     COST_FUNCTION_NODES_SETUP_DERIVATIVE = util.batch.universal.system.NodeSetup(memory=JOB_MEMORY_GB, node_kind='clmedium', nodes=4, cpus=32, nodes_max=8, check_for_better=True)
-    COST_FUNCTION_NODES_SETUP_TRAJECTORY = util.batch.universal.system.NodeSetup(memory=JOB_MEMORY_GB, node_kind='clexpress', nodes=1, cpus=1, nodes_max=1, walltime=2, check_for_better=True)
+    COST_FUNCTION_NODES_SETUP_TRAJECTORY = util.batch.universal.system.NodeSetup(memory=JOB_MEMORY_GB, node_kind='clmedium', nodes=1, cpus=1, nodes_max=1, walltime=2, check_for_better=True)
     COST_FUNCTION_NODES_SETUP_JOB = util.batch.universal.system.NodeSetup(memory=30, node_kind='clmedium', nodes=1, cpus=1, total_cpus_max=1, walltime=48, check_for_better=False)
