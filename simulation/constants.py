de1d4c253953c23b6c2f2cb29d9de23479891221
Joscha Reimer <jor@informatik.uni-kiel.de>
2019-10-03 18:27:58 +0200

    MAINT: simulation.constants: old unused constants removed

--- simulation/constants.py
+++ simulation/constants.py
@@ -2,19 +2,8 @@ import os.path
 
 import util.io.env
 
-
-BASE_DIR_ENV_NAME = 'NDOP_DIR'
-try:
-    BASE_DIR = util.io.env.load(BASE_DIR_ENV_NAME)
-    PARAMETER_OPTIMIZATION_DIR = os.path.join(BASE_DIR, 'parameter_optimization')
-    OED_DIR = os.path.join(BASE_DIR, 'optimal_experimental_design')
-except util.io.env.EnvironmentLookupError:
-    pass
-
-
 SIMULATION_OUTPUT_DIR_ENV_NAME = 'SIMULATION_OUTPUT_DIR'
 SIMULATION_OUTPUT_DIR = util.io.env.load(SIMULATION_OUTPUT_DIR_ENV_NAME)
 
 METOS3D_DIR_ENV_NAME = 'METOS3D_DIR'
 METOS3D_DIR = util.io.env.load(METOS3D_DIR_ENV_NAME)
-
--- simulation/optimization/job.py
+++ simulation/optimization/job.py
@@ -160,7 +160,7 @@ class CostFunctionJob(util.batch.universal.system.Job):
                 return ''
             else:
                 return 'export {env_name}={env_value}'.format(env_name=env_name, env_value=env_value)
-        env_names = [simulation.constants.BASE_DIR_ENV_NAME, simulation.constants.SIMULATION_OUTPUT_DIR_ENV_NAME, simulation.constants.METOS3D_DIR_ENV_NAME, measurements.constants.BASE_DIR_ENV_NAME, util.batch.universal.system.BATCH_SYSTEM_ENV_NAME, util.io.env.PYTHONPATH_ENV_NAME]
+        env_names = [simulation.constants.SIMULATION_OUTPUT_DIR_ENV_NAME, simulation.constants.METOS3D_DIR_ENV_NAME, measurements.constants.BASE_DIR_ENV_NAME, util.batch.universal.system.BATCH_SYSTEM_ENV_NAME, util.io.env.PYTHONPATH_ENV_NAME]
         pre_commands = [export_env_command(env_name) for env_name in env_names]
         pre_commands.append(batch_system.pre_command('python'))
 
