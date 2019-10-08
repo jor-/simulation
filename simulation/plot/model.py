6dac6593c153446db84c062c68e801efaff87578
Joscha Reimer <jor@informatik.uni-kiel.de>
2019-10-08 13:57:29 +0200

    ENH: simulation.plot.model_output: tracer argument added

--- simulation/plot/model.py
+++ simulation/plot/model.py
@@ -8,10 +8,14 @@ import simulation.model.cache
 import simulation.plot.util
 
 
-def output(model_options, time_dim, plot_type='all', v_max=None, overwrite=False, colorbar=True, **kwargs):
+def output(model_options, time_dim, tracer=None, plot_type='all', v_max=None, overwrite=False, colorbar=True, **kwargs):
     model = simulation.model.cache.Model(model_options=model_options)
     model_lsm = model.model_lsm
-    for tracer in model_options.tracers:
+    if tracer is None:
+        tracers = model_options.tracers
+    else:
+        tracers = (tracer,)
+    for tracer in tracers:
         f_all = model.f_all(time_dim, tracers=(tracer,))[tracer]
         base_file = simulation.plot.util.filename(model, 'model_output', tracer)
         measurements.plot.data.plot(f_all, base_file, model_lsm, plot_type=plot_type, v_max=v_max, overwrite=overwrite, colorbar=colorbar, **kwargs)
--- simulation/plot/model_output.py
+++ simulation/plot/model_output.py
@@ -14,6 +14,7 @@ def _main():
         simulation.util.args.argparse_add_model_options(parser)
 
         parser.add_argument('--time_dim', default=12, type=int, help='Desired time dim of model output.')
+        parser.add_argument('--tracer', default=None, help='The tracer that should be ploted. If not passed, all tracers are plotted.')
         parser.add_argument('--plot_type', default='all', help='Desired plot type.')
         parser.add_argument('--v_max', default=None, help='The maximal value used in the plot.')
         parser.add_argument('--no_colorbar', action='store_true', help='Do not plot colorbar.')
@@ -26,9 +27,6 @@ def _main():
 
         model_options = simulation.util.args.parse_model_options(args, concentrations_must_be_set=True, parameters_must_be_set=True)
 
-        time_dim = args.time_dim
-        plot_type = args.plot_type
-
         v_max = args.v_max
         if v_max is not None:
             try:
@@ -38,15 +36,16 @@ def _main():
 
         colorbar = not args.no_colorbar
 
-        overwrite = args.overwrite
-
         if args.kwargs is not None:
             kwargs = dict(kwarg.split('=') for kwarg in args.kwargs)
         else:
             kwargs = {}
 
         # plot
-        simulation.plot.model.output(model_options, time_dim, plot_type=plot_type, v_max=v_max, overwrite=overwrite, colorbar=colorbar, **kwargs)
+        simulation.plot.model.output(
+            model_options, args.time_dim,
+            tracer=args.tracer, plot_type=args.plot_type, v_max=v_max, overwrite=args.overwrite, colorbar=colorbar,
+            **kwargs)
 
 
 if __name__ == "__main__":
