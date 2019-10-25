c0dabc656c43e3083a493f1917cf021b211c2102
Joscha Reimer <jor@informatik.uni-kiel.de>
2019-10-25 12:41:47 +0200

    MAINT: simulation.plot.model: tick exponent notation updated

--- simulation/plot/model.py
+++ simulation/plot/model.py
@@ -67,22 +67,12 @@ def model_confidences(accuracy_object, matrix_type='F_H', alpha=0.99, include_va
     assert len(data) == len(tracers)
 
     # transform ticks
-    if v_max is not None:
-        v_max_for_tick_factor = v_max
-    else:
-        v_max_for_tick_factor = util.plot.auxiliary.v_max(data)
-    tick_exponent = int(np.floor(np.log10(v_max_for_tick_factor)))
-    if tick_exponent >= 0:
-        tick_exponent_str = f'e\\!\\!{tick_exponent}$'
-    else:
-        tick_exponent_str = f'e\\!\\!-\\!\\!{-tick_exponent}$'
-    tick_factor = 10**tick_exponent
+    tick_transform_exponent = util.plot.auxiliary.tick_transform_function_exponent_notation(data, v_max=v_max)
 
     def tick_transform(tick):
-        tick /= tick_factor
-        tick = int(np.round(tick, decimals=0))
-        tick_label = f'$\\pm{tick}{tick_exponent_str}'
-        return tick_label
+        tick = tick_transform_exponent(tick)
+        tick = '$\\pm' + tick[1:]
+        return tick
 
     if plot_type.startswith('depth'):
         tick_transform_dict = {'tick_transform_x': tick_transform}
@@ -97,17 +87,19 @@ def model_confidences(accuracy_object, matrix_type='F_H', alpha=0.99, include_va
         if tracer is None or tracer_i == tracer:
             plot_name = f'model_confidences_-_{tracer_i}_-_matrix_type_{matrix_type}_-_include_variance_factor_{include_variance_factor}_-_alpha_{alpha}_-_time_dim_model_{time_dim_model}_-_time_dim_confidence_{time_dim_confidence}'
             base_file = _filename_with_accuracy_object(accuracy_object, plot_kind, plot_name)
-            measurements.plot.data.plot(data[i], base_file, model_lsm, plot_type=plot_type, v_max=v_max, overwrite=overwrite, colorbar=colorbar, **tick_transform_dict, **kwargs)
+            measurements.plot.data.plot(data[i], base_file, model_lsm, plot_type=plot_type, v_max=v_max, overwrite=overwrite, colorbar=colorbar, tick_power_limit_scientific=100, **tick_transform_dict, **kwargs)
 
 
 def model_confidence_increases(accuracy_object, number_of_measurements=1, alpha=0.99, include_variance_factor=True,
                                relative_average_model_confidence_for_increases=True, increases_relative_to_average_model_confidence=True,
                                time_dim_model=12, time_dim_confidence_increase=12,
                                tracer=None, plot_type='all', v_max=None, overwrite=False, colorbar=True, **kwargs):
+    # check tracer
     tracers = accuracy_object.model.model_options.tracers
     if tracer is not None and tracer not in tracers:
         raise ValueError(f'Tracer {tracer} is unkown. Only the tracers {tracers} are in the model.')
 
+    # get data
     model_lsm = accuracy_object.model.model_lsm
     data = accuracy_object.average_model_confidence_increase(
         number_of_measurements=number_of_measurements, alpha=alpha, include_variance_factor=include_variance_factor,
@@ -115,9 +107,20 @@ def model_confidence_increases(accuracy_object, number_of_measurements=1, alpha=
         time_dim_model=time_dim_model, time_dim_confidence_increase=time_dim_confidence_increase)
     assert len(data) == len(tracers)
 
+    # transform ticks
+    tick_transform = util.plot.auxiliary.tick_transform_function_exponent_notation(data, v_max=v_max)
+
+    if plot_type.startswith('depth'):
+        tick_transform_dict = {'tick_transform_x': tick_transform}
+    elif plot_type in ('time', 'histogram'):
+        tick_transform_dict = {'tick_transform_y': tick_transform}
+    else:
+        tick_transform_dict = {'colorbar_tick_transform': tick_transform}
+
+    # plot
     plot_kind = 'average_model_confidence_increases'
     for i, tracer_i in enumerate(tracers):
         if tracer is None or tracer_i == tracer:
             plot_name = f'increases_-_{tracer_i}_-_measurements_{number_of_measurements}_-_relative_confidence_{relative_average_model_confidence_for_increases}_-_relative_increases_{increases_relative_to_average_model_confidence}_-_variance_factor_{include_variance_factor}_-_alpha_{alpha}_-_time_dim_model_{time_dim_model}_-_time_dim_increase_{time_dim_confidence_increase}'
             base_file = _filename_with_accuracy_object(accuracy_object, plot_kind, plot_name)
-            measurements.plot.data.plot(data[i], base_file, model_lsm, plot_type=plot_type, v_max=v_max, overwrite=overwrite, colorbar=colorbar, tick_power_limit_scientific=2, **kwargs)
+            measurements.plot.data.plot(data[i], base_file, model_lsm, plot_type=plot_type, v_max=v_max, overwrite=overwrite, colorbar=colorbar, tick_power_limit_scientific=100, **tick_transform_dict, **kwargs)
